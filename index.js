e87acdf4c377dc5b2f2e8f2ca5ec171f5b493910
= <rtyenriques@gmail.com>
2022-01-18 00:56:19 -0800

    added formhandler that has access to form data

--- index.js
+++ index.js
@@ -2,7 +2,19 @@ const endpoint = "http://localhost:3000/api/v1/entries"
 
 document.addEventListener('DOMContentLoaded', () => {
     getEntries()
+
+    const createEntryForm = document.querySelector('#create-entry-form')
+    createEntryForm.addEventListener('submit', (e) => createFormHandler(e))
 })
+
+function createFormHandler(e) {
+  e.preventDefault()
+  const nameInput = document.querySelector('#input-name').value
+  const crewInput =document.querySelector('#input-crew').value
+  const locationInput = document.querySelector('#input-location').value
+  const compInput = document.querySelector('#competitions').value
+  
+}
 
 function getEntries() {
     fetch(endpoint)
@@ -11,6 +23,7 @@ function getEntries() {
       json.data.forEach(entries => {
         const entryData = `
         <div data-id=${entries.id}>
+          <h2>${entries.attributes.competition.comp_type}
           <h3>${entries.attributes.name}</h3>
           <h3>${entries.attributes.crew}</h3>
           <h3>${entries.attributes.location}</h3>
