d436dfc10fbfdbd2b1ab7e7515ec184b2ab102fc
= <rtyenriques@gmail.com>
2022-01-18 15:24:31 -0800

    DRYed out render function

--- index.js
+++ index.js
@@ -7,8 +7,34 @@ document.addEventListener('DOMContentLoaded', () => {
     createEntryForm.addEventListener('submit', (e) => createFormHandler(e))
 })
 
+function getEntries() {
+    fetch(endpoint)
+    .then(response => response.json())
+    .then(json => {
+      json.data.forEach(entries => {
+     render(entries)
+        })
+    })
+    .catch(err => console.log(err))
+}
+
+function render(entries) {
+  const entryMarkup = `
+        <div data-id=${entries.id}>
+          <h2>${entries.attributes.competition.comp_type}
+          <h3>${entries.attributes.name}</h3>
+          <h3>${entries.attributes.crew}</h3>
+          <h3>${entries.attributes.location}</h3>
+          <button data-id=${entries.id}>edit</button>
+        </div>
+        <br>`;
+  document.querySelector('#entry-container').innerHTML +=
+  entryMarkup;
+}
+
 function createFormHandler(e) {
   e.preventDefault()
+
   const nameInput = document.querySelector('#input-name').value
   const crewInput =document.querySelector('#input-crew').value
   const locationInput = document.querySelector('#input-location').value
@@ -34,40 +60,12 @@ fetch(endpoint, {
 })
 .then(response => response.json())
 .then(entries => {
- 
-  // console.log(entries);
-  // const entryData = entries.data.attributes
-  const entryMarkup =`
-  <div data-id=${entries.id}>
-  <h3>${entries.name}</h3>
-  <h3>${entries.crew}</h3>
-  <h3>${entries.location}</h3>
-  <button data-id=${entries.id}>edit</button>
-  </div>
-  `;
-  document.querySelector('#entry-container').innerHTML +=
-  entryMarkup;
+
+  console.log(entries);
+  const entryData = entries.data
+render(entryData)
 
 })
 
 }
 
-function getEntries() {
-    fetch(endpoint)
-    .then(response => response.json())
-    .then(json => {
-      json.data.forEach(entries => {
-        const entryData = `
-        <div data-id=${entries.id}>
-          <h2>${entries.attributes.competition.comp_type}
-          <h3>${entries.attributes.name}</h3>
-          <h3>${entries.attributes.crew}</h3>
-          <h3>${entries.attributes.location}</h3>
-          <button data-id=${entries.id}>edit</button>
-        </div>
-        <br>`;
-        
-        document.querySelector('#entry-container').innerHTML += entryData
-        })
-    })
-}
