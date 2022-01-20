15533c92d6a53f95a8491b0983b59793a2cf0083
= <rtyenriques@gmail.com>
2022-01-19 23:17:54 -0800

    update

--- src/entry.js
+++ src/entry.js
@@ -1,32 +1,26 @@
 class Entry {
 constructor(entry, entryAttributes) {
-    // debugger
 this.id = entry.id;
 this.name = entryAttributes.name;
 this.crew = entryAttributes.crew;
 this.location = entryAttributes.location;
 this.competition = entryAttributes.competition;
 Entry.all.push(this)
-
 }
  renderEntry() {
-   
-return `
-        <div data-id=${this.id}>
-          <h2>${this.competition.comp_type}
-          <h3>${this.name}</h3>
-          <h3>${this.crew}</h3>
-          <h3>${this.location}</h3>
-          <button data-id=${this.id}>edit</button>
-        </div>
-        <br>`;
-
+   return `
+      <div data-id=${this.id}>
+      <h2>${this.competition.comp_type}
+      <h3>${this.name}</h3>
+      <h3>${this.crew}</h3>
+      <h3>${this.location}</h3>
+      <button data-id=${this.id}>edit</button>
+      </div>
+      <br>`;
 }
-
 static findById(id) {
-    return this.all.find(entry => entry.id === id)
+  return this.all.find(entry => entry.id === id)
 }
-
 }
 
 Entry.all = [];
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 const endpoint = "http://localhost:3000/api/v1/entries"
 
 document.addEventListener('DOMContentLoaded', () => {
-    getEntries()
-
-    const createEntryForm = document.querySelector('#create-entry-form')
-    createEntryForm.addEventListener('submit', (e) => createFormHandler(e))
+  getEntries()
+  const createEntryForm = document.querySelector('#create-entry-form')
+  createEntryForm.addEventListener('submit', (e) => createFormHandler(e))
 })
 
 function getEntries() {
@@ -12,34 +11,15 @@ function getEntries() {
     .then(response => response.json())
     .then(json => {
       json.data.forEach(entry => {
-
-      let newEntry = new Entry(entry.id, entry.attributes)
-
-    //  render(entry)
-    document.querySelector('#entry-container').innerHTML += newEntry.renderEntry()
-  ;
-        })
+        let newEntry = new Entry(entry.id, entry.attributes)
+        document.querySelector('#entry-container').innerHTML += newEntry.renderEntry();
+      })
     })
     // .catch(err => console.log(err))
 }
 
-function render(entries) {
-  const entryMarkup = `
-        <div data-id=${entries.id}>
-          <h2>${entries.attributes.competition.comp_type}
-          <h3>${entries.attributes.name}</h3>
-          <h3>${entries.attributes.crew}</h3>
-          <h3>${entries.attributes.location}</h3>
-          <button data-id=${entries.id}>edit</button>
-        </div>
-        <br>`;
-  document.querySelector('#entry-container').innerHTML +=
-  entryMarkup;
-}
-
 function createFormHandler(e) {
   e.preventDefault()
-
   const nameInput = document.querySelector('#input-name').value
   const crewInput =document.querySelector('#input-crew').value
   const locationInput = document.querySelector('#input-location').value
@@ -55,24 +35,15 @@ const bodyData = {name, crew, location, competition_id}
 fetch(endpoint, {
   method: 'POST',
   headers: {'Content-Type': 'application/json'},
-  // body: JSON.stringify({
-  //   name: name,
-  //   crew: crew,
-  //   location: location,
-  //   competition_id: competition_id
-  // })
   body: JSON.stringify(bodyData)
 })
 .then(response => response.json())
 .then(entry => {
-
   console.log(entry);
   const entryData = entry.data
   let newEntry = new Entry(entryData, entryData.attributes)
-
- document.querySelector('#entry-container').innerHTML +=
+  document.querySelector('#entry-container').innerHTML +=
   newEntry.renderEntry();
 })
-
 }
 
