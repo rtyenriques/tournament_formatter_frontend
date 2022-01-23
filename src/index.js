5181538c0b7a02a0af416b014f69aa3868682200
= <rtyenriques@gmail.com>
2022-01-23 14:38:38 -0800

    updates

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 const endpoint = "http://localhost:3000/api/v1/entries"
+// const entryContainer = document.querySelector('#entry-container')
+
 
 document.addEventListener('DOMContentLoaded', () => {
-    getEntries()
 
+    getEntries()
+   
     const createEntryForm = document.querySelector('#create-entry-form')
     createEntryForm.addEventListener('submit', (e) => createFormHandler(e))
+
+    // const editEntryForm = document.querySelector("entry-container")
+    // editEntryForm.addEventListener('click', e => {
+    // console.log('clicked');
+    // })
 })
 
 function getEntries() {
@@ -23,19 +31,19 @@ function getEntries() {
     // .catch(err => console.log(err))
 }
 
-function render(entries) {
-  const entryMarkup = `
-        <div data-id=${entries.id}>
-          <h2>${entries.attributes.competition.comp_type}
-          <h3>${entries.attributes.name}</h3>
-          <h3>${entries.attributes.crew}</h3>
-          <h3>${entries.attributes.location}</h3>
-          <button data-id=${entries.id}>edit</button>
-        </div>
-        <br>`;
-  document.querySelector('#entry-container').innerHTML +=
-  entryMarkup;
-}
+// function render(entries) {
+//   const entryMarkup = `
+//         <div data-id=${entries.id}>
+//           <h2>${entries.attributes.competition.comp_type}
+//           <h3>${entries.attributes.name}</h3>
+//           <h3>${entries.attributes.crew}</h3>
+//           <h3>${entries.attributes.location}</h3>
+//           <button data-id=${entries.id}>edit</button>
+//         </div>
+//         <br>`;
+//   document.querySelector('#entry-container').innerHTML +=
+//   entryMarkup;
+// }
 
 function createFormHandler(e) {
   e.preventDefault()
@@ -55,12 +63,6 @@ const bodyData = {name, crew, location, competition_id}
 fetch(endpoint, {
   method: 'POST',
   headers: {'Content-Type': 'application/json'},
-  // body: JSON.stringify({
-  //   name: name,
-  //   crew: crew,
-  //   location: location,
-  //   competition_id: competition_id
-  // })
   body: JSON.stringify(bodyData)
 })
 .then(response => response.json())
@@ -73,6 +75,7 @@ fetch(endpoint, {
  document.querySelector('#entry-container').innerHTML +=
   newEntry.renderEntry();
 })
-
 }
+
+
 
