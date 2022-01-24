b6de351673a391ae4e3c5006b62e25a0bae49bf3
= <rtyenriques@gmail.com>
2022-01-24 13:19:45 -0800

    cleaned up code spacing for js index

--- src/index.js
+++ src/index.js
@@ -1,100 +1,69 @@
 const endpoint = "http://localhost:3000/api/v1/entries"
 const entryList = () => document.getElementById('entry-container')
-// const entryContainer = document.querySelector('#entry-container')
-
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  getEntries()
-  const createEntryForm = document.querySelector('#create-entry-form')
-  createEntryForm.addEventListener('submit', (e) => createFormHandler(e));
+getEntries();
+const createEntryForm = document.querySelector('#create-entry-form');
+createEntryForm.addEventListener('submit', (e) => createFormHandler(e));
   
-  const fourVfour = document.querySelector('#four')
-
-  fourVfour.addEventListener('click', e => {
-    e.preventDefault()
-    resetList()
-    Entry.sortByComp()
+const fourVfour = document.querySelector('#four');
+fourVfour.addEventListener('click', e => {
+  e.preventDefault()
+  resetList()
+  Entry.sortByComp()
   })
   
-  const allComps = document.querySelector('#all')
-  allComps.addEventListener('click', e => {
+const allComps = document.querySelector('#all');
+allComps.addEventListener('click', e => {
   e.preventDefault()
   resetList()
   Entry.allComps()
   })
 
-  const oneOnone = document.querySelector('#one')
-  oneOnone.addEventListener('click', e => {
-    e.preventDefault()
-    resetList()
-    Entry.oneAdult()
+const oneOnone = document.querySelector('#one');
+oneOnone.addEventListener('click', e => {
+  e.preventDefault()
+  resetList()
+  Entry.oneAdult()
   })
 
-const oneOnoneKids = document.querySelector('#one-kid')
- oneOnoneKids.addEventListener('click', e => { 
+const oneOnoneKids = document.querySelector('#one-kid');
+oneOnoneKids.addEventListener('click', e => { 
   e.preventDefault()
   resetList()
   Entry.oneKids()
+  })
+
 })
-})
 
-
-  // const fourVfour = document.querySelector('#four')
-  // debugger
-  // fourVfour.addEventListener('click', console.log('hi'))
-  
-
-
-
-  const resetList = () => {
+const resetList = () => {
   entryList().innerHTML = ''
 }
 
-
-
-function getEntries() {
-   fetch(endpoint)
-    .then (response => response.json())
-    .then (json => {
-      json.data.forEach(entry => {
-        let newEntry = new Entry(entry, entry.attributes)
-        // document.querySelector('#entry-container').innerHTML += newEntry.renderEntry();
-        newEntry.renderEntry()
+async function getEntries() {
+  await fetch(endpoint)
+  .then (response => response.json())
+  .then (json => {
+    json.data.forEach(entry => {
+      let newEntry = new Entry(entry, entry.attributes);
+      newEntry.renderEntry()
       })
     })
-    // .catch(err => console.log(err))
+    .catch(err => console.log(err))
 }
-
-
-// function render(entries) {
-//   const entryMarkup = `
-//         <div data-id=${entries.id}>
-//           <h2>${entries.attributes.competition.comp_type}
-//           <h3>${entries.attributes.name}</h3>
-//           <h3>${entries.attributes.crew}</h3>
-//           <h3>${entries.attributes.location}</h3>
-//           <button data-id=${entries.id}>edit</button>
-//         </div>
-//         <br>`;
-//   document.querySelector('#entry-container').innerHTML +=
-//   entryMarkup;
-// }
-
 
 function createFormHandler(e) {
   e.preventDefault()
-  const nameInput = document.querySelector('#input-name').value
-  const crewInput =document.querySelector('#input-crew').value
-  const locationInput = document.querySelector('#input-location').value
-  const compInput = document.querySelector('#competitions').value
-  const compId = parseInt(compInput)
-  // const compId = parseInt(document.querySelector('#input-location').value)
+  const nameInput = document.querySelector('#input-name').value;
+  const crewInput =document.querySelector('#input-crew').value;
+  const locationInput = document.querySelector('#input-location').value;
+  const compInput = document.querySelector('#competitions').value;
+  const compId = parseInt(compInput);
   postFetch(nameInput, crewInput,locationInput, compId)
 }
 
 async function postFetch(name, crew, location, competition_id) {
-// console.log(name, crew, location, comp_id)
 const bodyData = {name, crew, location, competition_id}
 await fetch(endpoint, {
   method: 'POST',
@@ -103,17 +72,14 @@ await fetch(endpoint, {
 })
 .then(response => response.json())
 .then(entry => {
-  console.log(entry);
-  const entryData = entry.data
-  let newEntry = new Entry(entryData, entryData.attributes)
-  // document.querySelector('#entry-container').innerHTML +=
+  const entryData = entry.data;
+  let newEntry = new Entry(entryData, entryData.attributes);
   newEntry.renderEntry();
-  // debugger
-  const nameInput = document.querySelector('#input-name').value = ''
-  const crewInput =document.querySelector('#input-crew').value = ''
-  const locationInput = document.querySelector('#input-location').value = ''
+  const nameInput = document.querySelector('#input-name').value = '';
+  const crewInput =document.querySelector('#input-crew').value = '';
+  const locationInput = document.querySelector('#input-location').value = '';
   const compInput = document.querySelector('#competitions').value = ''
-})
+  })
 }
 
 
